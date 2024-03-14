072a9af8d70eb6fae011d67eade28ddd9d13aeeb
Arish Arsalan <159803393+Arish-Arsalan@users.noreply.github.com>
2024-03-14 20:53:23 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+//exersice 13
+let traportation = ['civic', 'bike', 'car', 'bus', 'suzuki'];
+for (let i = 0; i < traportation.length; i++) {
+    console.log('I would like to own a ' + traportation[i]);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+//exersice 13
+let traportation : string[] = ['civic','bike','car','bus','suzuki'];
+for(let i=0; i<traportation.length; i++){
+    console.log('I would like to own a ' + traportation[i]);
+}
